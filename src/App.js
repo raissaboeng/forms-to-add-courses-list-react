3d2d890cb79daab39f4d064111a5496f18d6de2b
raissaboeng <raissa.boeng@gmail.com>
2021-01-25 01:27:10 -0300

    Create component Course and Function remove

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Course from './components/course';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state = {
+      courses: [
+        {
+          id: '1',
+          name: 'React',
+          category: 'Javascript',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
+        },
+        {
+          id: '2',
+          name: 'React',
+          category: 'Javascript',
+          image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
+        }
+      ]
+    }
+    this.remove = this.remove.bind(this);
+  }
+
+  remove(courseId){
+    const { courses } = this.state,
+      courseIndex = courses.findIndex(course => course.id = courseId);
+
+      courses.splice(courseIndex, 1);
+      this.setState({courses});
+
+
+  }
+
+  render(){
+    const {state} = this;
+     return (
+      <div className="App">
+        <ul className="courses-list">
+         {
+           state.courses.map(course => <Course course={course} onRemove={this.remove}/>)
+         }
+        </ul>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
